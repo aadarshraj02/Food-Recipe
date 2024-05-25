f2eae509db5db47f6bb57803ddd6b58a1ed51e15
aadarshraj02 <aadarshraazz007@gmail.com>
2024-05-25 21:01:32 +0530

    added a function that will fetch and give data of food recipes

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const searchBox = document.querySelector(".searchBox");
 const recipeContainer = document.querySelector(".recipe-container");
 const searchBtn = document.querySelector(".searchBtn");
 
+const fetchRecipes = async()=>{
+    
+}
+
 searchBtn.addEventListener("click",(e)=>{
     e.preventDefault();
 })
