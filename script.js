08b163fa584f3fe4a40aa872949b46e2ef6a04d2
aadarshraj02 <aadarshraazz007@gmail.com>
2024-05-25 21:10:49 +0530

    fixed the bug that was .value and console.log to check the output in console

--- script.js
+++ script.js
@@ -6,10 +6,11 @@ const fetchRecipes = async (query) => {
   const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`;
   const data = await fetch(url);
   const response = await data.json();
+  console.log(response);
 };
 
 searchBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  const searchInput = searchBox.ariaValueMax.trim();
+  const searchInput = searchBox.value.trim();
   fetchRecipes(searchInput);
 });
