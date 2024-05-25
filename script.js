b7e21f70eb4ca3bf97082b0f66a72f01e06573c7
aadarshraj02 <aadarshraazz007@gmail.com>
2024-05-25 21:13:28 +0530

    added for each loop on meal to get array of meals

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ const fetchRecipes = async (query) => {
   const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`;
   const data = await fetch(url);
   const response = await data.json();
-  console.log(response);
+  //   console.log(response);
+  response.meals.forEach((meal) => {});
 };
 
 searchBtn.addEventListener("click", (e) => {
