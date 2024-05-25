bbd7f2dd50621ecf54c50c6af0c32cb1304e7ee4
aadarshraj02 <aadarshraazz007@gmail.com>
2024-05-25 23:25:53 +0530

    added try catch to handle error

--- script.js
+++ script.js
@@ -7,29 +7,33 @@ const closeBtn = document.querySelector(".recipe-close-btn");
 const fetchRecipes = async (query) => {
   recipeContainer.innerHTML = "Fetching Recipes...";
   const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`;
-  const data = await fetch(url);
-  const response = await data.json();
-  //   console.log(response);
+  try {
+    const data = await fetch(url);
+    const response = await data.json();
+    //   console.log(response);
 
-  recipeContainer.innerHTML = "";
-  response.meals.forEach((meal) => {
-    const recipeDiv = document.createElement("div");
-    recipeDiv.classList.add("recipe");
-    recipeDiv.innerHTML = `
+    recipeContainer.innerHTML = "";
+    response.meals.forEach((meal) => {
+      const recipeDiv = document.createElement("div");
+      recipeDiv.classList.add("recipe");
+      recipeDiv.innerHTML = `
         <img src = "${meal.strMealThumb}">
         <h3>${meal.strMeal}</h3>
         <p><span>${meal.strArea}</span> Dish</p>
         <p>Belongs to <span>${meal.strCategory}</span> Category</p>
     `;
-    const button = document.createElement("button");
-    button.innerHTML = "View Recipe";
-    recipeDiv.appendChild(button);
-    recipeContainer.appendChild(recipeDiv);
+      const button = document.createElement("button");
+      button.innerHTML = "View Recipe";
+      recipeDiv.appendChild(button);
+      recipeContainer.appendChild(recipeDiv);
 
-    button.addEventListener("click", () => {
-      openRecipePopup(meal);
+      button.addEventListener("click", () => {
+        openRecipePopup(meal);
+      });
     });
-  });
+  } catch (error) {
+    recipeContainer.innerHTML = "Error in Fetching Recipes...";
+  }
 };
 
 const fetchIngredients = (meal) => {
