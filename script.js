43f0ce3032de2a2f959412843cdb55ec5282475d
aadarshraj02 <aadarshraazz007@gmail.com>
2024-05-25 21:08:38 +0530

    updated function to async and called function

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ const searchBox = document.querySelector(".searchBox");
 const recipeContainer = document.querySelector(".recipe-container");
 const searchBtn = document.querySelector(".searchBtn");
 
-const fetchRecipes = async()=>{
-    
-}
+const fetchRecipes = async (query) => {
+  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`;
+  const data = await fetch(url);
+  const response = await data.json();
+};
 
-searchBtn.addEventListener("click",(e)=>{
-    e.preventDefault();
-})
+searchBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const searchInput = searchBox.ariaValueMax.trim();
+  fetchRecipes(searchInput);
+});
