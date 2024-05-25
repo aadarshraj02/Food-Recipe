54e157899a7737b90ae5f629dc71489720bd4a12
aadarshraj02 <aadarshraazz007@gmail.com>
2024-05-25 19:47:35 +0530

    added event listener on search button

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const searchBox = document.querySelector(".searchBox");
 const recipeContainer = document.querySelector(".recipe-container");
 const searchBtn = document.querySelector(".searchBtn");
+
+searchBtn.addEventListener("click",(e)=>{
+    e.preventDefault();
+})
